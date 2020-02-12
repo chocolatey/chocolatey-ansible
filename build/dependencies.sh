ea7906710dccd14be9c477bb218feb4baf2210a9
Joel Francis <32407840+vexx32@users.noreply.github.com>
2020-02-12 09:23:45 -0500

    :rocket: add missing python3-dev package

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-sudo apt-get install python3 python3-pip python3-venv -y
+sudo apt-get install python3 python3-pip python3-venv python3-dev -y
 
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
