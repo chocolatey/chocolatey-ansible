976352fc59dc4ebe91dc19fc2dde563e3a2f5d6f
Joel Francis <32407840+vexx32@users.noreply.github.com>
2020-02-12 15:12:03 -0500

    :rocket: remove ansible-test explicit install

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -7,4 +7,4 @@ sudo apt-get install python3 python3-pip python3-venv python3-dev -y
 python3 -m venv ~/ansible-venv
 source ~/ansible-venv/bin/activate
 pip3 install --upgrade wheel
-pip3 install ansible ansible-test pywinrm
+pip3 install ansible pywinrm
