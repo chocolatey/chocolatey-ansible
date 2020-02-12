82b43561d912a7e35f0c3734988cd0272ede1a70
Joel Francis <32407840+vexx32@users.noreply.github.com>
2020-02-12 14:54:35 -0500

    :rocket: Don't add ansible repo to apt

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -4,9 +4,6 @@ sudo apt-get update
 
 sudo apt-get install python3 python3-pip python3-venv python3-dev -y
 
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-
 python3 -m venv ~/ansible-venv
 source ~/ansible-venv/bin/activate
 pip3 install --upgrade wheel
