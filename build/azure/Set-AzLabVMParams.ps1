07c48ce0d4e6255a5c63402f483ec8d158d6bc1f
Joel Francis <32407840+vexx32@users.noreply.github.com>
2020-02-25 14:26:48 -0500

    :rocket: Add self-signed certificate to Az VM

--- build/azure/Set-AzLabVMParams.ps1
+++ build/azure/Set-AzLabVMParams.ps1
@@ -31,3 +31,40 @@ Write-Host "##vso[task.setvariable variable=ChocoCIClient.labVMIpAddress;]$labVM
 
 Write-Host "Setting Lab VM FQDN Var: $labVMFqdn"
 Write-Host "##vso[task.setvariable variable=ChocoCIClient.labVMFqdn;]$labVMFqdn"
+
+$CertificateScript = {
+    # Make sure the Fully Qualified Domain Name is being used
+    $hostName = [System.Net.Dns]::GetHostName()
+    $domainName = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().DomainName
+
+    If (-Not $hostName.endswith($domainName)) {
+        $hostName += "." + $domainName
+    }
+
+    Write-Host "Creating self signed certificate"
+
+    # you can only generate a new certificate in 'My'
+    # necessary to branch based on PowerShell version, since not all parameters are supported in earlier versions
+    if ($PSVersionTable.PSVersion.Major -le 4) {
+        $newCert = New-SelfSignedCertificate -CertStoreLocation cert:\LocalMachine\My -DnsName $hostName
+    }
+    else {
+        $newCert = New-SelfSignedCertificate -CertStoreLocation cert:\LocalMachine\My -DnsName $hostName -KeyUsage KeyEncipherment, DigitalSignature -NotAfter (Get-Date).AddYears(10)
+    }
+
+    # move the certificate to 'TrustedPeople'
+    $certPath = Get-ChildItem -Path 'Cert:\\LocalMachine\\My' | Where-Object subject -like "*$hostName"
+    Move-Item -Path $certPath.PsPath -Destination 'Cert:\\LocalMachine\\TrustedPeople' > $null
+}
+
+$Script = New-Item -Path ./CertScript.ps1
+$CertificateScript | Set-Content -Path $Script.FullName
+
+$params = @{
+    ResourceGroupName = 'choco-ci'
+    VMName            = $labVMName
+    CommandId         = 'RunPowerShellScript'
+    ScriptPath        = $Script.FullName
+}
+
+Invoke-AzVMRunCommand @params -Verbose
