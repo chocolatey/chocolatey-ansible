a8704d7def386fd442ec0cf5c6152a32d4eda75d
Stephen Valdinger <stephen@chocolatey.io>
2020-04-12 10:40:07 -0400

    Shims a package useful for asserting pkg params work
    ## Usage
    
    * `choco pack pipeline.nuspec`
    * `choco install pipeline -y --package-parameters="'/File=foo.txt'"`
    * `choco uninstall pipeline -y --package-parameters="'/File=foo.txt'"`
    
    This should install a file called foo.txt to $env:TEMP, and similarly remove it
    
    Adjust a `--source` parameter as necesary.

---
+++ build/Chocolatey/pipeline/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$pp = Get-PackageParameters
+
+$null = New-Item -Path $env:TEMP -Name $pp['File'] -ItemType File
+Write-Host "Created '$($pp['File'])' in '$env:TEMP'"
---
+++ build/Chocolatey/pipeline/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$pp = Get-PackageParameters
+
+Remove-Item "$env:TEMP\$($pp['File'])" -Force
+Write-Host "Removed '$($pp['File'])' from '$env:TEMP'"
