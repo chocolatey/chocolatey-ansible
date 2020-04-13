2a1459203cc268b63ec729025bd9c449e636a170
Stephen Valdinger <stephen@chocolatey.io>
2020-04-13 10:56:31 -0400

    Forgot to delete my original files. Whoops. Fixed!

--- build/Chocolatey/pipeline/tools/chocolateyinstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-
-$pp = Get-PackageParameters
-
-$null = New-Item -Path $env:TEMP -Name $pp['File'] -ItemType File
-Write-Host "Created '$($pp['File'])' in '$env:TEMP'"
--- build/Chocolatey/pipeline/tools/chocolateyuninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-
-$pp = Get-PackageParameters
-
-Remove-Item "$env:TEMP\$($pp['File'])" -Force
-Write-Host "Removed '$($pp['File'])' from '$env:TEMP'"
