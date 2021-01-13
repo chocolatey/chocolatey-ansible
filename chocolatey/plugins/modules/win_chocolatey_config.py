aefbc952fb2ef3edf83516bbd776339da9b0758a
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2021-01-13 10:40:37 -0500

    (#30) :memo: Fix doc linting issues
    - Rewrite old versions to 0.x.x so they match semver and kind of make
      sense for the current version of the modules being 1.x.
    - Fix doc argument spec issues.

--- chocolatey/plugins/modules/win_chocolatey_config.py
+++ chocolatey/plugins/modules/win_chocolatey_config.py
@@ -12,7 +12,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = r'''
 ---
 module: win_chocolatey_config
-version_added: '2.7'
+version_added: '0.2.7'
 short_description: Manages Chocolatey config settings
 description:
 - Used to manage Chocolatey config settings as well as unset the values.
