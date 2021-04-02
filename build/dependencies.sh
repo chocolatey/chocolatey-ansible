fac28b727ba6384ed946e8500fbe73d1303b1201
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2021-04-02 10:54:14 -0400

    :construction: ensure pip is installed in the server
    Apparently pip is missing, somehow. Not really sure how/why on that one.

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+sudo add-apt-repository universe
+sudo apt update
 sudo apt-get update
 
 sudo apt-get install python3 python3-pip python3-venv python3-dev -y
+sudo apt install python2
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+
+pip2 --version
+pip3 --version
 
 python3 -m venv ~/ansible-venv
 source ~/ansible-venv/bin/activate
