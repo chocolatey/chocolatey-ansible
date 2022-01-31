50163ee24a98846a094ee8c7849bcb8923f4a998
Joel Sallow <32407840+vexx32@users.noreply.github.com>
2022-01-31 12:41:43 -0500

    (maint) Add cryptography dependency for 2.11
    Also fixed up a few issues in the vagrant box configuration for testing
    purposes.

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 sudo add-apt-repository universe
-sudo apt update
-sudo apt install software-properties-common
-
 sudo apt-get update
+sudo apt-get install software-properties-common
 sudo apt-get install python3 python3-pip python3-venv python3-dev -y
 
 python3 -m venv ~/ansible-venv
 source ~/ansible-venv/bin/activate
 
 pip3 install --upgrade wheel
+pip3 install 'pyOpenSSL<22.0.0'
 pip3 install pywinrm
-pip3 install $ANSIBLE_PACKAGE
+pip3 install "$ANSIBLE_PACKAGE"
 
 pip3 --version
 ansible --version
---
+++ build/vagrant-dependencies.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+sudo add-apt-repository universe
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-get install python3 python3-pip python3-venv python3-dev -y
+sudo pip install virtualenv
+
+virtualenv /home/vagrant/ansible-venv
+. /home/vagrant/ansible-venv/bin/activate
+
+pip3 install --upgrade wheel
+pip3 install 'pyOpenSSL<22.0.0'
+pip3 install pywinrm
+pip3 install "$ANSIBLE_PACKAGE"
+
+# Ensure pwsh is installed (Ansible requires this for some sanity tests)
+sudo apt-get install -y wget apt-transport-https software-properties-common
+# Download the Microsoft repository GPG keys
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
+# Install PowerShell
+sudo apt-get install -y powershell
+
+pip3 --version
+ansible --version
